140f28b7071ae860c2e09d072bf4487642e3fe80
Yawen Qian <qian91328@gmail.com>
2020-03-11 23:17:17 -0500

    Add dependency injections

--- spring-demo-one/src/springdemo/TrackCoach.java
+++ spring-demo-one/src/springdemo/TrackCoach.java
@@ -1,10 +1,26 @@
 package springdemo;
 
 public class TrackCoach implements Coach {
+	
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {
+	}
+	
+	// define a constructor for dependency injection
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Just do it " + fortuneService.getFortune();
 	}
 
 }
