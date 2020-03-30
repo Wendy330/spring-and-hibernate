69aa1800bca0ba4da012f0f45d52c484af1616de
Yawen Qian <qian91328@gmail.com>
2020-03-29 23:43:19 -0500

    Add DAO classes

--- web-customer-tracker/src/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/springdemo/controller/CustomerController.java
@@ -1,15 +1,24 @@
 package springdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import springdemo.dao.CustomerDAO;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	@Autowired
+	private CustomerDAO customerDAO;
 
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		theModel.addAttribute("customers", customerDAO.getCustomers());
+		
 		return "list-customers";
 	}
 }
---
+++ web-customer-tracker/src/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,27 @@
+package springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		return sessionFactory.getCurrentSession()
+							 .createQuery("from Customer", Customer.class)
+							 .getResultList();
+	}
+
+}
