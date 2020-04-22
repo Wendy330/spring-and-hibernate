11c5342118423dec62aba3475ddc12609ab106a7
Yawen Qian <qian91328@gmail.com>
2020-04-21 23:11:10 -0500

    Pointcut expressions

--- spring-demo-aop/src/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/aopdemo/MainDemoApp.java
@@ -3,6 +3,7 @@ package aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import aopdemo.dao.AccountDAO;
+import aopdemo.dao.MembershipDAO;
 
 public class MainDemoApp {
 
@@ -12,9 +13,11 @@ public class MainDemoApp {
 		
 		// Get the bean from Spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class); 
 		
 		// Call the business method
 		theAccountDAO.addAccount();
+		theMembershipDAO.addAccount();
 		
 		// Close the context
 		context.close();
