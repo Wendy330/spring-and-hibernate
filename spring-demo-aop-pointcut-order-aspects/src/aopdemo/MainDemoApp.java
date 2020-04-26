ca9773ca5058c6a1427132436d6654bb0405224b
Yawen Qian <qian91328@gmail.com>
2020-04-26 13:51:58 -0500

    AOP ordering aspects

---
+++ spring-demo-aop-pointcut-order-aspects/src/aopdemo/MainDemoApp.java
@@ -0,0 +1,36 @@
+package aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import aopdemo.dao.AccountDAO;
+import aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		// Read Spring config Java Class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// Get the bean from Spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class); 
+		
+		// Call the business method
+		theAccountDAO.addAccount(new Account(), true);
+		theAccountDAO.doWork();
+		
+		theAccountDAO.setName("foobar");
+		theAccountDAO.setServiceCode("silver");
+		
+		String name = theAccountDAO.getName();
+		String code = theAccountDAO.getServiceCode();
+		
+		
+		theMembershipDAO.addAccount();
+		theMembershipDAO.goToSleep();
+		
+		// Close the context
+		context.close();
+	}
+
+}
