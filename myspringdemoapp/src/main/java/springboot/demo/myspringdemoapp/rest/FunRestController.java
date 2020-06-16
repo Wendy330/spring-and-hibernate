397f4393c875be87869167fccf48a0165ac9ca0a
Yawen Qian <qian91328@gmail.com>
2020-06-16 16:02:57 -0500

    Set up spring boot demo app

---
+++ myspringdemoapp/src/main/java/springboot/demo/myspringdemoapp/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package springboot.demo.myspringdemoapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	// Expose "/" that return "Hello World"
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! Time on server is " + LocalDateTime.now();
+	}
+}
