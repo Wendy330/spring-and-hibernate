394fee015f8435eb2fe953f69235722e3e44650c
Yawen Qian <qian91328@gmail.com>
2020-06-17 00:43:42 -0500

    Add actuator demo app

---
+++ 03-actuator-demo/src/test/java/springboot/demo/myspringdemoapp/MyspringdemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.demo.myspringdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringdemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
