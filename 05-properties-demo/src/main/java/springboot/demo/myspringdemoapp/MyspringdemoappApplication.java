ec2af5ea963a77d96ae5a3c44a2981192ddc0ebb
Yawen Qian <qian91328@gmail.com>
2020-06-19 00:11:15 -0500

    Add properties demo app

---
+++ 05-properties-demo/src/main/java/springboot/demo/myspringdemoapp/MyspringdemoappApplication.java
@@ -0,0 +1,13 @@
+package springboot.demo.myspringdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringdemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringdemoappApplication.class, args);
+	}
+
+}
